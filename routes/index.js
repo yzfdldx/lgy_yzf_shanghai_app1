628b40174da233003a1fd05ce7bd3136cf830ae2
yzfdldx <yzf@126.com>
2019-12-23 21:06:31 +0800

    okfdas

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/test.json', function(req, res, next) { // 查看详情
+router.get('/test1.json', function(req, res, next) { // 查看详情
   try {
     const mysql = require('mysql');
     // const query = req.query;
@@ -15,6 +15,47 @@ router.get('/test.json', function(req, res, next) { // 查看详情
       password: 'lgy987654321',
     });
     pool.getConnection((err, connecting) => {
+      if (err) {
+        res.send({
+          result: 'error',
+          errorCode: err,
+          message: '数据库连接失败',
+        });
+      } else { // 链接成功
+        res.send({
+          result: 'succeed',
+          data: 'ok',
+        });
+      }
+    });
+  } catch (error) {
+    res.send({
+      result: 'error',
+      errorCode: error,
+      message: '未知错误',
+    });
+  }
+});
+
+router.get('/test.json', function(req, res, next) { // 查看详情
+  try {
+    // const query = req.query;
+    const query = req.body;
+    let mssql = require('mssql');
+    let db = {};
+    let config = {
+        user: 'sa',
+        password: 'lgy987654321',  //刚刚改好的密码
+        server: 'DESKTOP-9G3CN68',  
+        port: 1433,
+        database: 'AXData',
+        pool: {
+            min: 0,
+            max: 10,
+            idleTimeoutMillis: 3000
+        }
+    };
+    let connection = new mssql.ConnectionPool(config, function (err) {
       if (err) {
         res.send({
           result: 'error',
